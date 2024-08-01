e7c65b3829d5a04a187806c1a7854ab81add0b11
Alex Hunsley <alex.hunsley@gmail.com>
2024-08-01 22:09:33 +0100

    Putting topic stuff in ProtWitPipe file (split out!)

--- Sources/Other_stuff/ProtWitPipe.swift
+++ Sources/Other_stuff/ProtWitPipe.swift
@@ -86,100 +86,230 @@ func testo(_: (a: Int, b: String)) -> String {
 
 /// -------------------------------------------------------------------------------------
 
+/*
+ Topics
+
+ Problem:
+
+ You have different enums or representations that represent a given aspect of a single enum or subject.
+
+ For example, a given screen in an app can have actions (screenViewed or screenClosed), and then also events
+ that details the same things. Rather than have a set of enums (Action, Event, etc) that have the same things in them, we want
+ to define things like screenViewed or screenClosed just once, and then attach that to Actions, Events etc in such
+ a way that the latter things are decoupled from each other.
 
 
-// TODO I was here
+ topic, facets, context? (facets are the cases for a topic, basicallym and context is like action or event.)
+ */
+
 
 public protocol TopicRepresentable {
-    associatedtype Topic
+    associatedtype Topic: Equatable
 
-    var top: Topic { get }
-//    func myTopic() -> Topic
-//    func top() -> Topic
-        //    var topic: String { get }
-//    func topic() -> Topic
-//    func topic<OtherType: TopicRepresentable>() -> OtherType
-
-    init(top: Topic)
+    var topic: Topic { get }
+    init(topic: Topic)
 }
 
 extension TopicRepresentable {
+    // init style
+    public init<T: TopicRepresentable>(topicHolder: T) where T.Topic == Self.Topic {
+        self.init(topic: topicHolder.topic)
+    }
 
+    // non-init style
     func topic<OtherType: TopicRepresentable>() -> OtherType where OtherType.Topic == Self.Topic {
-//        OtherType.init(topic: myTopic())
-        OtherType.init(top: top)
+        OtherType.init(topic: topic)
+    }
+
+//    func equals<OtherType: TopicRepresentable>(lhs: Self, rhs: OtherType) -> Bool where OtherType.Topic == Self.Topic, Self.Topic: Equatable {
+//        lhs.topic == rhs.topic
+//    }
+
+    func equals<OtherType: TopicRepresentable>(_ other: OtherType) -> Bool where OtherType.Topic == Self.Topic {
+        topic == other.topic
     }
 }
 
-enum LoginTopic {
+// Idea not working currently. Was wanting to just add action and event calc props to any
+// `enum XTopic` with a protocol declaration.
+//protocol AsAction { }
+//
+//extension AsAction where Self: TopicRepresentable {
+//    var action: LoginAction {
+//        LoginAction(topic: self)
+//    }
+//}
+
+//enum LoginTopic: Equatable, AsAction {
+enum LoginTopic: Equatable {
     case screenViewed
     case screenDismissed
     case userLoggedIn
+    case userLoggedOut
 }
 
-//struct LoginAction: TopicRepresentable<Topic> {
+protocol Wooga {
+    static var thingy: String { get }
+}
+
+//protocol WoogaProviding: Wooga { }
+//
+//extension WoogaProviding {
+//    static var thingy: String { String(describing: self) }
+//}
+
+extension Wooga {
+    static var thingy: String { String(describing: self) }
+}
+
+enum Context {
+    enum Action: Wooga { }
+    enum Event: Wooga { }
+
+    //    enum Action: WoogaProviding { }
+//    enum Event: WoogaProviding { }
+
+    //    enum Action: Wooga {
+//        static var thingy: String { "action" }
+//    }
+
+
+//    enum Event {
+//        static var thingy: String { "event" }
+//    }
+//    case action
+//    case event
+}
+
+typealias Action = Context.Action
+typealias Event = Context.Event
+
+//protocol Doober { }
+//
+//extension Doober {
+//    func changer<Other: Doober>() {
+//        Other
+//    }
+//}
+
+//enum LoginTopic2<Aspect: Wooga>: Doober, Equatable {
+enum LoginTopic2<Aspect: Wooga>: Equatable {
+//    var t: Type { Aspect.Type }
+    var aspect: String { Aspect.thingy }
+
+    case screenViewed
+    case screenDismissed
+    case userLoggedIn
+    case userLoggedOut
+}
+
+let t: LoginTopic2<Action> = .screenViewed
+
+// TODO get it converting, same as original code below, to a different aspect with same Topic (e.g. screenViewed)
+//let t2: LoginTopic2<Event> = t.changer
+
+
+///----------------------------------------------------------
+
+/// Thing2:
+
+// can't do this, good
+//let x: LoginTopic2<Event> = t
+
+//protocol Actionable { }
+//extension Actionable {
+//    var action: any TopicRepresentable {
+//        Self(topic: )
+//    }
+//}
+
+extension LoginTopic {
+    var action: LoginAction {
+        LoginAction(topic: self)
+    }
+
+    var event: LoginEvent {
+        LoginEvent(topic: self)
+    }
+
+//    func action() -> LoginAction {
+//        LoginAction(topic: self)
+//    }
+
+//    func event() -> LoginEvent {
+//        LoginEvent(topic: self)
+//    }
+}
+
+// no way to make this an enum too?
+// struct is still a value type tho, so not so bad.
 struct LoginAction: TopicRepresentable {
-    typealias Topic = LoginTopic
-
-    var top: Topic
-
-//    func myTopic() -> LoginTopic {
-//        top
-//    }
+    var topic: LoginTopic
 }
 
+//struct Action<T>: TopicRepresentable {
+//    var topic: LoginTopic
+//}
+
+//enum LocationActionEnum {
+//    // calc prop?
+//    var topic: LoginTopic {
+//        
+//    }
+//}
+
+//struct LoginEvent: TopicRepresentable, Equatable {
 struct LoginEvent: TopicRepresentable {
-    typealias Topic = LoginTopic
-
-    var top: Topic
-
-//    func myTopic() -> LoginTopic {
-//        top
-//    }
-
-    public init(top: Topic) {
-        self.top = top
-    }
-
-    // can we make a builder for this (static) and put into the extension?
-    public init<T: TopicRepresentable>(topicHolder: T) where T.Topic == Self.Topic {
-        top = topicHolder.top
-    }
+    var topic: LoginTopic
 }
-
-
 
 public func do_it() {
+    let loginAction = LoginAction(topic: .screenViewed)
 
-    // topics idea
+    // or:
+    let loginActionMakeTwo = .screenViewed.action
 
-    let loginAction = LoginAction.init(top: .screenViewed)
-
-    // method one
+    // method one: non-init.
+    // The generic magic lets us call .topic on a topic holder and create any other
+    // holder with same topic, implicitly.
     let loginEvent: LoginEvent = loginAction.topic()
 
-    // method two
-    let loginEvent2: LoginEvent = .init(topicHolder: loginAction)
+    // method two: init
+    let loginEvent2 = LoginEvent(topicHolder: loginAction)
 
-//    let loginRepr: TopicRepresentable<LoginTopic> = loginAction
-    //    let loginEvent: LoginEvent = loginRepr.topic()
+    let loginAction2 = LoginAction(topicHolder: loginEvent2)
+
     print(loginEvent)
     print(loginEvent2)
+    print(loginAction2)
 
-    switch loginEvent.top {
+    // checking equality
+        // not quite got this working yet.
+//    if loginEvent == loginAction { }
+    // this works though. Must access the
+    if loginEvent.topic == loginAction.topic {
+        print("They are the same")
+    }
+    // also works
+    if loginEvent.equals(loginAction) {
+        print("They are the same, check 2")
+
+    }
+
+    // accessing a topic
+    switch loginEvent.topic {
     case .screenViewed:
         break
     case .screenDismissed:
         break
     case .userLoggedIn:
         break
+    case .userLoggedOut:
+        break
     }
-    // need a way to make uncoupled things into each other using the topic.
-    // e.g. action into a logger
 
-//    let loginAction = LoginAction.screenViewed
-//
-//    let loginEvent: LoginEvent = loginAction.topic()
+    print("t = ", t)
+    print("t.aspect = ", t.aspect)
 }
 
 // OSLog demo (works)
